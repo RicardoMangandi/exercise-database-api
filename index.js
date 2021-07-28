2fb5a7b8b03dbb42ae0710c71662347a632c803a
Ricardo Mangandi <ricardo.mangandi@gmail.com>
2021-07-27 22:59:59 -0400

    commit

--- index.js
+++ index.js
@@ -9,14 +9,10 @@ let port = process.env.PORT || 3000
 app.get("/", (req,res) => {
 
         res.send("Hello World")
-
-
 })
 
 app.get("/exercises", (req,res) => {
-
     res.send(importData)
-
 })
 
 
